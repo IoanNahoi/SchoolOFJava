cbe2b8f57f505f02f79cc27e9a8680d6d7f11e5b
inahoi <ioan.nahoi@endava.com>
2022-05-31 11:37:10 +0300

    created a list of elements

---
+++ SchoolOFJavaGIT/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> lista_elemente=new ArrayList<>();
+        lista_elemente.add(5);
+        lista_elemente.add(10);
+        lista_elemente.add(5132);
+        lista_elemente.add(5112);
+        lista_elemente.add(215);
+    }
+}
